e871265917a146649b2adb414fad0454453c93e8
sam_glb <samwang@test.com>
2020-02-23 14:53:00 +0800

    test conflict

--- src/main/java/testgit/happy.java
+++ src/main/java/testgit/happy.java
@@ -4,6 +4,7 @@ public class happy {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("next");
+        System.out.println("left");
     }
 
 }
